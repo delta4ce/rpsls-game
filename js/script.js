1f001dd53a3293e343003bd9a068df37427786e9
Alex <147667539+delta4ce@users.noreply.github.com>
2023-10-26 15:14:56 +0200

    Added score display

--- js/script.js
+++ js/script.js
@@ -8,13 +8,6 @@
         // Paper disproves Spock
         // Spock vaporizes Rock
         // And, as it always has, Rock crushes Scissors
-const choices = ["Rock", "Paper", "Scissors", "Lizard", "Spock"]
-let roundString = '';
-let stringCh = 0;
-
-let playerScore = 0; //implement scoring system
-let compScore = 0;
-
 const btn1 = document.querySelector('.buttons button:nth-child(1)')
 btn1.addEventListener('click', () => playRound(btn1.innerText))
 const btn2 = document.querySelector('.buttons button:nth-child(2)')
@@ -26,13 +19,15 @@ btn4.addEventListener('click', () => playRound(btn4.innerText))
 const btn5 = document.querySelector('.buttons button:nth-child(5)')
 btn5.addEventListener('click', () => playRound(btn5.innerText))
 
-function getChoice(number) {
-  return choices[number]
-}
+
+let playerScore = 0;
+let compScore = 0;
+
 
 function playRound(playerChoice) {
+	const choices = ["Rock", "Paper", "Scissors", "Lizard", "Spock"]
 	stringCh = 0;
-	document.querySelector('h2').textContent = '';
+
 	const Rules = {
 	  Rock: ["Scissors", "Lizard"],
 	  Paper: ["Rock", "Spock"],
@@ -40,17 +35,33 @@ function playRound(playerChoice) {
 	  Lizard: ["Spock", "Paper"],
 	  Spock: ["Rock", "Scissors"],
 	};
-	const compChoice = getChoice(Math.floor(Math.random() * choices.length));
+
+	const compChoice = choices[Math.floor(Math.random() * choices.length)];
 
 	if (playerChoice === compChoice) {
 		roundString = "It's a tie!";
 	} else if (Rules[playerChoice].includes(compChoice)) {
-		roundString = `You win! - ${getString(playerChoice, compChoice)}`;
+		roundString = `You win! - ${getString(playerChoice, compChoice)}.`;
 		playerScore++;
 	} else {
-		roundString = `I win with a ${compChoice}! - ${getString(playerChoice, compChoice)}`;
+		roundString = `I win with a ${compChoice}! - ${getString(playerChoice, compChoice)}.`;
 		compScore++;
 	}
+
+	document.querySelector('.score').textContent = `Score: ${compScore} - ${playerScore}`;
+	
+	if (compScore === 5) {
+		roundString += ` Game over - I win! Play again?`
+		compScore = 0;
+		playerScore = 0;
+	}
+	if (playerScore === 5) {
+		roundString += ` Game over - You win! Play again?`
+		compScore = 0;
+		playerScore = 0;
+	}
+
+	document.querySelector('.string').textContent = '';
 	typeString();
 }
 
@@ -75,9 +86,12 @@ function getString(playerChoice, compChoice) {
 	}
 }
 
+let roundString = '';
+let stringCh = 0;
+
 function typeString() {
   if (stringCh < roundString.length) {
-    document.querySelector('h2').textContent += roundString[stringCh];
+    document.querySelector('.string').textContent += roundString[stringCh];
     stringCh++;
     setTimeout(typeString, 50);
   }
